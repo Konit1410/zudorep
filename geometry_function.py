f9a0cb15078b83ba7f63d5160874d56ff4704a12
Konit <nick.rapacz@gmail.com>
2020-08-02 15:41:20 +0200

    task1 added new function

---
+++ geometry_function.py
@@ -0,0 +1,13 @@
+class SpecialFunction:
+
+    #Calculating the middle of the line
+    def middle_section(num):
+        result = num / 2
+        return result
+
+    #Check for collisions between two rectangles
+    def collision_between_rectangles():
+        pass
+
+k = SpecialFunction
+print(k.middle_section(4.5))
