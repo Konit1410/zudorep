ea87fef5c83613750a31f5b8e818c66cb4fb9feb
Zuzanna Rams <Zuzannarams97@gmail.com>
2020-08-12 18:53:30 +0200

    Pliki do master

---
+++ Special_math.py
@@ -0,0 +1,31 @@
+import math
+import sys
+
+
+class Simple_math:
+
+    def sq_root_sum_by_product(arguments: list) -> complex():
+        """square root of sum divided by product from setpoint of numbers"""
+        ggg = 1
+        if len(arguments) != 0:
+            for i in arguments:
+                if i !=0:
+                    ggg = ggg * i
+            return (sum(arguments) / ggg) ** 2
+
+
+    def median_multiply_constants(n_num: list) -> complex:
+        """median multiplay by have PI and added Epsilion constant"""
+        EPSILON_CONSTANT = sys.float_info.epsilon
+        HAVE_PI = math.pi / 2
+        n = len(n_num)
+        n_num.sort()
+        if n != 0:
+            if n % 2 == 0:
+                median1 = n_num[n // 2]
+                median2 = n_num[n // 2 - 1]
+                median = (median1 + median2) / 2
+                return median * HAVE_PI + EPSILON_CONSTANT
+            else:
+                median = n_num[n // 2]
+                return median * HAVE_PI + EPSILON_CONSTANT
---
+++ Test_Simple_math.py
@@ -0,0 +1,27 @@
+import pytest
+from Task1.Special_math import Simple_math
+
+
+@pytest.mark.parametrize('arguments, ans', [
+    ([1, 2, 3], 1),
+    ([1], 1),
+    ([1, 1, 2, 3, 3], 0.308641975308642),
+    ([], None),
+    ([11, 23, 45, 68, 90], 1.156985131470954e-11),
+    ([-1, -2], 2.25),
+    ([0], 0)
+])
+def test_parameterize_01(arguments, ans):
+    assert Simple_math.sq_root_sum_by_product(arguments) == ans
+
+
+@pytest.mark.parametrize('n_num, ans', [
+    ([1, 2, 3], 3.141592653589793),
+    ([2, 3, 4, 5, 6], 6.283185307179586),
+    ([], None),
+    ([2, 4, 6, 7, 8, 9, 10], 10.995574287564276),
+    ([-1, -2, -3, -4], -3.9269908169872414),
+    ([0], 2.220446049250313e-16)
+])
+def test_parameterize_02(n_num, ans):
+    assert Simple_math.median_multiply_constants(n_num) == ans
